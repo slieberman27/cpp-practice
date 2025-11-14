1678c1ff4c35051bd3a348ea0603e5c6f7136022
slieberman27 <slieberman@browning.edu>
2025-11-14 17:55:40 +0000

    made it work but no user input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+// NOW MAKE IT USE USER INPUT
+
 int main() {
-  cout << "Hello World!";
+  double basePrice = 19.99;
+
+  const double taxRate = 0.085;
+
+  int amount = 3;
+
+  double subTotal = amount * basePrice;
+
+  double taxAmount = subTotal * taxRate;
+  
+  double total = taxAmount + subTotal;
+
+  cout << "For " << amount << " items at $" << basePrice << " each, the subtotal is $" << subTotal << ", tax is $" << taxAmount << " and total is $" << total << ".";
   return 0;
 }
